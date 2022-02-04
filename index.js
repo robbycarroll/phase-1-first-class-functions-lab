5b3ab0bcb0ca8dad9e1a9a3eaadbbf7bdc6ce0f1
Robert Carroll <robertcarrollsales@gmail.com>
2022-02-03 21:34:02 -0500

    commit tree ec417f5b62b17dccdfb36987f2e7442fb4a97955

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = () => ['Antonia', 'Nuru'];
+const returnLastTwoDrivers = () => ['Amari', 'Mo'];
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    }
+}
+
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (arrayOfDrivers, func) => func(arrayOfDrivers);
+
+
+ 
+
